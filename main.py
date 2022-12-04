c901ee38471a5b055ef6b3bc698aa91b9dfda8a1
lusta <75475914+AnastasiaLusta@users.noreply.github.com>
2022-12-04 13:39:58 +0000

    feat: add University class

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from random import *
+
+class University:
+	def __init__(self, title, faculty):
+		self.title = title
+		self.faculty = faculty
+		self.budget = False
+	def studying(self, name):
+		print(name, 'is studying')
+	def isbudget(self):
+		if self.budget == True:
+			print('Congrats! You are on budget')
+		else:
+			print('Pay money and study!')
+
 class Student:
 	def __init__(self, name):
 		self.name = name
 		self.gladness = 50
 		self.progress = 0
 		self.alive = True
+	def ask_budget(self):
+		if self.progress >= 20:
+			univer.budget = True
 	def say_hello(self):
 		print('Hello!')
 	def to_study(self):
 		print('Time to study')
-		self.progress += 5
+		self.progress += 15
 		self.gladness -= 10
 	def to_sleep(self):
 		print('Sleep time')
@@ -30,29 +47,28 @@ class Student:
 			self.alive = False
 	def statistics(self):
 		print('Gladness = ', self.gladness, ' Progress = ', self.progress)
+		print('Budget = ', univer.budget)
 	def live(self, day):
 		day = 'Day ' + str(day) + ' of ' + self.name + ' life'
 		print(day)
 		live_cube = randint(1,4)
 		if live_cube == 1:
+			self.ask_budget()
 			self.to_study()
+			univer.studying(self.name)
 		elif live_cube == 2:
 			self.to_sleep()
 		elif live_cube == 3:
 			self.to_chill()
 		elif live_cube == 4:
-			self.say_hello()
+				self.say_hello()
 		self.statistics()
 		self.is_alive()
 
+univer = University('Step', 'Computer Science')
 human = Student('Anton')
 for day in range(10):
 	if human.alive == False:
 		break
 	human.live(day)
 
-obj = Student('Harry')
-for day in range(10):
-	if obj.alive == False:
-		break
-	obj.live(day)
